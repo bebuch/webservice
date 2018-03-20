081448cf3c489dc2fd9a2b34c453f5b0d712d9d6
Benjamin Roland Buch <benni.buch@gmail.com>
2018-03-20 15:36:46 +0100

    refactoring

--- src/checked_ws_handler_base.cpp
+++ src/checked_ws_handler_base.cpp
@@ -7,25 +7,118 @@
 // file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 //-----------------------------------------------------------------------------
 #include <webservice/checked_ws_handler_base.hpp>
+#include <webservice/server.hpp>
 
-#include "checked_ws_handler_base_impl.hpp"
+#include "ws_session.hpp"
+
+#include <shared_mutex>
 
 
 namespace webservice{
 
 
 
+	/// \brief Implementation of checked_ws_handler_base
+	struct checked_ws_handler_base_impl{
+		/// \brief Send a message to all sessions
+		template < typename Data >
+		void send(Data const& data){
+			std::shared_lock< std::shared_timed_mutex > lock(mutex_);
+			for(auto const& session: sessions_){
+				send(session, data);
+			}
+		}
+
+		/// \brief Send a message to session by identifier
+		template < typename Data >
+		void send(
+			std::set< std::uintptr_t > const& identifiers,
+			Data const& data
+		){
+			std::shared_lock< std::shared_timed_mutex > lock(mutex_);
+			auto iter = identifiers.begin();
+			auto const end = identifiers.end();
+			for(auto const session: sessions_){
+				auto const identifier =
+					reinterpret_cast< std::uintptr_t >(session);
+
+				while(iter != end && *iter != identifier){
+					++iter;
+				}
+
+				if(iter == end){
+					break;
+				}
+
+				send(session, data);
+				++iter;
+			}
+		}
+
+		/// \brief Send a message to session by identifiers
+		template < typename Data >
+		void send(
+			std::uintptr_t const identifier,
+			Data&& data
+		){
+			std::shared_lock< std::shared_timed_mutex > lock(mutex_);
+			auto const iter = sessions_.find(reinterpret_cast<
+				ws_server_session* >(identifier));
+			if(iter == sessions_.end()){
+				return;
+			}
+
+			send(*iter, static_cast< Data&& >(data));
+		}
+
+
+		/// \brief Send a message to session
+		template < typename Data >
+		void send(
+			ws_server_session* const session,
+			Data&& data
+		){
+			session->send(static_cast< Data&& >(data));
+		}
+
+
+		/// \brief If it is true, new sessions are closed immediately
+		std::atomic< bool > shutdown_{false};
+
+		/// \brief Protect sessions_
+		///
+		/// We don't need the timed-part, but C++14 has no non-timed
+		/// shared_mutex.
+		std::shared_timed_mutex mutex_;
+
+		/// \brief List of sessions registered by open, erased by close
+		std::set< ws_server_session* > sessions_;
+	};
+
+
+
 	checked_ws_handler_base::checked_ws_handler_base()
-		: impl_(std::make_unique< checked_ws_handler_base_impl >(*this)){}
+		: impl_(std::make_unique< checked_ws_handler_base_impl >()){}
 
 
 	checked_ws_handler_base::~checked_ws_handler_base(){
-		impl_->shutdown();
+		shutdown();
 	}
 
 
 	void checked_ws_handler_base::shutdown()noexcept{
-		impl_->shutdown();
+		if(impl_->shutdown_.exchange(true)){
+			impl_->send("handler shutdown");
+		}
+
+		for(;;){
+			std::unique_lock< std::shared_timed_mutex > lock(impl_->mutex_);
+			if(impl_->sessions_.empty()){
+				break;
+			}
+			lock.unlock();
+			server().run_one();
+		}
 	}
 
 
@@ -63,14 +156,27 @@ namespace webservice{
 		ws_server_session* const session,
 		std::string const& resource
 	){
-		impl_->on_open(session, resource);
+		if(!impl_->shutdown_){
+			std::unique_lock< std::shared_timed_mutex > lock(impl_->mutex_);
+			impl_->sessions_.emplace(session);
+			lock.unlock();
+
+			auto identifier = reinterpret_cast< std::uintptr_t >(session);
+			on_open(identifier, resource);
+		}else{
+			session->rebind(nullptr);
+			session->send("handler shutdown");
+		}
 	}
 
 	void checked_ws_handler_base::on_close(
 		ws_server_session* const session,
 		std::string const& resource
 	){
-		impl_->on_close(session, resource);
+		on_close(reinterpret_cast< std::uintptr_t >(session), resource);
+
+		std::unique_lock< std::shared_timed_mutex > lock(impl_->mutex_);
+		impl_->sessions_.erase(session);
 	}
 
 	void checked_ws_handler_base::on_text(
@@ -78,7 +184,10 @@ namespace webservice{
 		std::string const& resource,
 		boost::beast::multi_buffer const& buffer
 	){
-		impl_->on_text(session, resource, buffer);
+		on_text(
+			reinterpret_cast< std::uintptr_t >(session),
+			resource,
+			buffer);
 	}
 
 	void checked_ws_handler_base::on_binary(
@@ -86,7 +195,10 @@ namespace webservice{
 		std::string const& resource,
 		boost::beast::multi_buffer const& buffer
 	){
-		impl_->on_binary(session, resource, buffer);
+		on_binary(
+			reinterpret_cast< std::uintptr_t >(session),
+			resource,
+			buffer);
 	}
 
 	void checked_ws_handler_base::on_error(
@@ -95,7 +207,11 @@ namespace webservice{
 		ws_handler_location location,
 		boost::system::error_code ec
 	){
-		impl_->on_error(session, resource, location, ec);
+		on_error(
+			reinterpret_cast< std::uintptr_t >(session),
+			resource,
+			location,
+			ec);
 	}
 
 	void checked_ws_handler_base::on_exception(
@@ -103,7 +219,10 @@ namespace webservice{
 		std::string const& resource,
 		std::exception_ptr error
 	)noexcept{
-		impl_->on_exception(session, resource, error);
+		on_exception(
+			reinterpret_cast< std::uintptr_t >(session),
+			resource,
+			error);
 	}
 
 
