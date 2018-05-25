ebaf6953ad069803de8a0935144fc27fcf5021ba
Benjamin Roland Buch <benni.buch@gmail.com>
2018-05-25 18:05:21 +0200

    maked interface more flexible

--- include/webservice/file_request_handler.hpp
+++ include/webservice/file_request_handler.hpp
@@ -26,17 +26,29 @@ namespace webservice{
 		void operator()(http_request&& req, http_response&& send)override;
 
 
+		/// \brief Get HTTP document root directory
 		std::string const& doc_root()const;
+
+		/// \brief Concat relative path with document root directory
+		///
+		/// This also adds index.html if the target ended on slash.
+		std::string with_path(boost::beast::string_view target)const;
 
 
 	protected:
-
 		virtual void on_file_not_found(
 			http_request&& req,
 			http_response&& send
 		);
 
-		void send_body(
+		/// \brief Make sure we can handle the method
+		static bool is_unknown_http_method(http_request const& req);
+
+		/// \brief Request path must be absolute and not contain "..".
+		static bool is_illegal_request_target(http_request const& req);
+
+		/// \brief Send the given file body as answer
+		static void send_body(
 			http_request&& req,
 			http_response&& send,
 			boost::beast::http::file_body::value_type&& body,
--- src/file_request_handler.cpp
+++ src/file_request_handler.cpp
@@ -22,30 +22,17 @@ namespace webservice{
 	){
 		namespace http = boost::beast::http;
 
-		// Make sure we can handle the method
-		if(
-			req.method() != http::verb::get &&
-			req.method() != http::verb::head
-		){
+		if(is_unknown_http_method(req)){
 			send(bad_request(req, "Unknown HTTP-method"));
 			return;
 		}
 
-		// Request path must be absolute and not contain "..".
-		if(
-			req.target().empty() ||
-			req.target()[0] != '/' ||
-			req.target().find("..") != boost::beast::string_view::npos
-		){
+		if(is_illegal_request_target(req)){
 			send(bad_request(req, "Illegal request-target"));
 			return;
 		}
 
-		// Build the path to the requested file
-		std::string path = path_concat(doc_root(), req.target());
-		if(req.target().back() == '/'){
-			path.append("index.html");
-		}
+		std::string path = with_path(req.target());
 
 		// Attempt to open the file
 		boost::beast::error_code ec;
@@ -72,11 +59,38 @@ namespace webservice{
 		return doc_root_;
 	}
 
+	std::string file_request_handler::with_path(
+		boost::beast::string_view target
+	)const{
+		std::string path = path_concat(doc_root(), target);
+		if(target.back() == '/'){
+			path.append("index.html");
+		}
+		return path;
+	}
+
 	void file_request_handler::on_file_not_found(
 		http_request&& req,
 		http_response&& send
 	){
 		send(not_found(req, req.target()));
+	}
+
+	bool file_request_handler::is_unknown_http_method(http_request const& req){
+		namespace http = boost::beast::http;
+
+		return
+			req.method() != http::verb::get &&
+			req.method() != http::verb::head;
+	}
+
+	bool file_request_handler::is_illegal_request_target(
+		http_request const& req
+	){
+		return
+			req.target().empty() ||
+			req.target()[0] != '/' ||
+			req.target().find("..") != boost::beast::string_view::npos;
 	}
 
 	void file_request_handler::send_body(
